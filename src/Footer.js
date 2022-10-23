89d63702c40ecf23189be931d9c567e5b2db9acb
José Antonio <juninho.juze@gmail.com>
2022-10-23 17:30:02 -0300

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="App-footer">
+      © 2022 Space Exploration Technologies Corporation. SpaceX and SpaceX logo are trademarks and/or registered trademarks of Space Exploration Technologies Corporation in the US and/or other countries.
+    </footer>
+  );
+}
+
+export default Footer;
