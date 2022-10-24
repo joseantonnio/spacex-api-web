8d1755f399a796faf243b328f61a21519e590ac1
José Antonio <juninho.juze@gmail.com>
2022-10-24 18:54:03 -0300

    Add React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+      </Routes>
       <Footer />
     </div>
   );
