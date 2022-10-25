6ac6de02184487dcdf8a3451669b2b5485d0253a
José Antonio <juninho.juze@gmail.com>
2022-10-24 22:25:43 -0300

    Adds next lauches page

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,12 @@ import { RotatingLines } from  'react-loader-spinner'
 
 class Card extends React.Component {
   render() {
+    const loading = this.props.loading || false;
+
     return (
       <div class="App-card">
         <div class="App-card-bg" style={{ backgroundImage: `url(${this.props.bg})` }}></div>
-        {this.props.loading ? <RotatingLines strokeColor="white" width="36" /> : this.props.children}
+        {loading ? <RotatingLines strokeColor="white" width="36" /> : this.props.children}
       </div>
     );
   }
